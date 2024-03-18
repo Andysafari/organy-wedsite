97f5e4adc9f80678ab70e9e9b87359fe7a43f2f1
Andysafari <152931685+Andysafari@users.noreply.github.com>
2024-03-18 16:19:59 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+    alert ( "hello, world " );  
+
+    let age = 16; 
+
+    const birthyear = 2007 
+
+    // console.log ('' MY age is ''  + age + '' and I was born in '' + birthyear + '' )    
+
+    // if ( age > 18) 
+    //   console.log ( 'you are an adult') 
+    // { 
+    // else{   
+    //   console.log ( 'you are a child""')
+    //   }  
+
+    // if ( age > 18 && age < 30 ) { console.log(" you are in twenties or late teens")}  
+
+    // ( age > 18 || age < 30 ) { console.log(" you are under 18 or over 30")} 
+
+    //   let isminor = age < 18;
+    //   console.log (" you are not a minor" + isminor) 
+
+
