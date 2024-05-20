8975b820b8618a900a34d0f215fd6a4d349601bb
Andysafari <152931685+Andysafari@users.noreply.github.com>
2024-05-20 20:42:58 +0000

    css /script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+    alert ( "welcome to organ " );  
+
+    let age = 16; 
+
+    let birthyear = 2007 
+
+
+   document.getElementById("footer").innerText = `${new Date ().getFullYear()}`;
+
+    console.log (" MY age is "  + age + " and I was born in" + birthyear)  
+
+    if ( age > 18) {
+      console.log ( 'you are an adult') 
+    } 
+    else{   
+      console.log ( 'you are a child""')
+      }  
+
+    // if ( age > 18 && age < 30 ) { console.log(" you are in twenties or late teens")}  
+
+    // ( age > 18 || age < 30 ) { console.log(" you are under 18 or over 30")} 
+
+    //   let isminor = age < 18;
+    //   console.log (" you are not a minor" + isminor) 
+
+
